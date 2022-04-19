41c4c362096a919c24acd9df03e783e6cc8120a2
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-04-20 00:42:34 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navi from "./Navigate"
+import ProductList from "./ProductList";
+function App() {
+  return (
+    <div >
+      <header >
+      <h1>selamlar</h1>
+      <Navi /> 
+      <ProductList />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
