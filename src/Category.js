be3adeb6fb92667ef9efde86924b52a678984aae
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-04-24 03:10:41 +0300

    state and events practice

--- src/Category.js
+++ src/Category.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
-import {ListGroup, ListGroupItem } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: "1", name: "Teknoloji" },
+        { categoryId: "2", name: "Giyim" },
+        { categoryId: "3", name: "Ayakkabı" },
+        { categoryId: "4", name: "Müzik" },
+        { categoryId: "5", name: "Spor" },
+      ],
+      selectedCategory:""
+    };
+  }
+  showSelectedCategory= (category) =>{
+    this.setState({ selectedCategory: category.name})
+  }
   render() {
     return (
       <div>
-      <h2>{this.props.title}</h2>
+        <h2>{this.props.title}</h2>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem onClick={() => this.showSelectedCategory(category)} key={category.categoryId}>
+              {category.name}
+            </ListGroupItem>
+          ))}
         </ListGroup>
+        <h4>Selected Category : {this.state.selectedCategory}</h4>
       </div>
     );
   }
